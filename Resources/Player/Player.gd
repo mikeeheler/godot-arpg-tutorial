12db3090bef5c994bae571d208e985e82953187b
Mike Eheler <mike.eheler@gmail.com>
2020-04-10 19:38:54 -0700

    Initial commit

---
+++ Resources/Player/Player.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+const ACCELERATION = 500
+const FRICTION = 500
+const MAX_SPEED = 80
+
+var velocity = Vector2.ZERO
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+func _physics_process(delta):
+    var input_vector = Vector2.ZERO
+    input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+    input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+    
+    if input_vector.length() > 0.1:
+        animationTree.set("parameters/Idle/blend_position", input_vector)
+        animationTree.set("parameters/Run/blend_position", input_vector)
+        animationState.travel("Run")
+        velocity = velocity.move_toward(input_vector.normalized() * MAX_SPEED, ACCELERATION * delta)
+    else:
+        animationState.travel("Idle")
+        velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+    
+    velocity = move_and_slide(velocity)
