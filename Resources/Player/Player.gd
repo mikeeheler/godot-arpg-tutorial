30083ae40f43842950c6c0daab24e71a407df9e9
Mike Eheler <mike.eheler@gmail.com>
2020-04-11 10:42:51 -0700

    Grass, collisions, etc

--- Resources/Player/Player.gd
+++ Resources/Player/Player.gd
@@ -4,13 +4,39 @@ const ACCELERATION = 500
 const FRICTION = 500
 const MAX_SPEED = 80
 
+enum {
+    MOVE,
+    ROLL,
+    ATTACK
+}
+
+var state = MOVE
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
+func _ready():
+    animationTree.active = true
+
 func _physics_process(delta):
+    match state:
+        MOVE:
+            move_state(delta)
+        ROLL:
+            pass
+        ATTACK:
+            attack_state(delta)
+    
+func attack_state(_delta):
+    velocity = Vector2.ZERO
+    animationState.travel("Attack")
+    
+func attack_animation_finished():
+    state = MOVE
+
+func move_state(delta):
     var input_vector = Vector2.ZERO
     input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
     input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -18,6 +44,7 @@ func _physics_process(delta):
     if input_vector.length() > 0.1:
         animationTree.set("parameters/Idle/blend_position", input_vector)
         animationTree.set("parameters/Run/blend_position", input_vector)
+        animationTree.set("parameters/Attack/blend_position", input_vector)
         animationState.travel("Run")
         velocity = velocity.move_toward(input_vector.normalized() * MAX_SPEED, ACCELERATION * delta)
     else:
@@ -25,3 +52,6 @@ func _physics_process(delta):
         velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
     
     velocity = move_and_slide(velocity)
+    
+    if Input.is_action_just_pressed("player_attack"):
+        state = ATTACK
---
+++ Resources/World/Grass.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+func _on_Hurtbox_area_entered(area):
+    create_grass_effect()
+    queue_free()
+
+
+func create_grass_effect():
+    var GrassEffect = load("res://Resources/Effects/GrassEffect.tscn")
+    var grassEffect = GrassEffect.instance()
+    var world = get_tree().current_scene
+    world.add_child(grassEffect)
+    grassEffect.global_position = global_position
